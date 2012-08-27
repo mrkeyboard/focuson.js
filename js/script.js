8a4c02fff7203ec642b46a66a7648575329b2ab1
Ido <ido@wecreatestuff.com>
2012-08-27 07:57:17 +0300

    test

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 var tutorial = "<div style='width:500px; height:200px; background: white'>hello world</div>"
 
-$("h2").guidejs({padding: 0, html: tutorial, position: 'rightudc84k'});
+$("h2").guidejs({padding: 0, html: tutorial, position: 'right'});
 $("#logo").guidejs({padding: 20, timer:2000});
 //$("h3").guidejs {padding: 5, html:'blah <a href="#" class="guidejs-next">nigga</a>'}
 
